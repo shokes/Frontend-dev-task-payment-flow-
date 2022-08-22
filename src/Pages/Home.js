1157f4a2f731716771427358df128fdadfb87ca7
Oshoke <oyatioshoke@yahoo.com>
2022-08-22 09:25:42 +0100

    bug

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,5 +19,4 @@ const Home = () => {
     </section>
   );
 };
-
 export default Home;
--- src/context.js
+++ src/context.js
@@ -4,13 +4,13 @@ const AppContext = React.createContext();
 
 const AppProvider = function ({ children }) {
   const storedValueAsNumber = Number(localStorage.getItem('id'));
-  const [id, setId] = useState(
-    Number.isInteger(storedValueAsNumber) ? storedValueAsNumber : 1
-  );
+  const [id, setId] = useState(Number.isInteger(1) ? storedValueAsNumber : 1);
 
   useEffect(() => {
     localStorage.setItem('id', String(id));
   }, [id]);
+
+  // const [id, setId] = useState(1);
 
   return (
     <AppContext.Provider value={{ setId, id }}>{children}</AppContext.Provider>
