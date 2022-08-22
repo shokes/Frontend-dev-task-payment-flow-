5599801fe032d04f07b840c9aabd6e497ecff500
Oshoke <oyatioshoke@yahoo.com>
2022-08-22 09:32:49 +0100

    bug

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import GoToTop from '../GoToTop';
 import PersonalInfoForm from '../Components/PersonalInfoForm/PersonalInfoForm';
-import { useGlobalContext } from '../context';
-import BillingInfoForm from '../Components/BillingInfoForm/BillingInfoForm';
-import PurchaseForm from '../Components/PurchaseForm/PurchaseForm';
 
 const Home = () => {
-  const { setId, id } = useGlobalContext();
-  if (id === 2) {
-    return <BillingInfoForm />;
-  } else if (id === 3) {
-    return <PurchaseForm />;
-  }
   return (
     <section className='container'>
       <PersonalInfoForm />
--- src/context.js
+++ src/context.js
@@ -3,14 +3,14 @@ import React, { useContext, useState, useEffect } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = function ({ children }) {
-  const storedValueAsNumber = Number(localStorage.getItem('id'));
-  const [id, setId] = useState(Number.isInteger(1) ? storedValueAsNumber : 1);
+  // const storedValueAsNumber = Number(localStorage.getItem('id'));
+  // const [id, setId] = useState(Number.isInteger(1) ? storedValueAsNumber : 1);
 
-  useEffect(() => {
-    localStorage.setItem('id', String(id));
-  }, [id]);
+  // useEffect(() => {
+  //   localStorage.setItem('id', String(id));
+  // }, [id]);
 
-  // const [id, setId] = useState(1);
+  const [id, setId] = useState(1);
 
   return (
     <AppContext.Provider value={{ setId, id }}>{children}</AppContext.Provider>
